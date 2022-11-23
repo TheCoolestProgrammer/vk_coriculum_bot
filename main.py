138948d53b21c8fcbe12643f195c09444e3fe5eb
TheCoolestProgrammer <81899758+TheCoolestProgrammer@users.noreply.github.com>
2022-11-23 12:03:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-import os
 import time
 
 import vk_api
 import bs4
 import requests
-from flask import Flask
 from vk_api.utils import get_random_id
-from schedule import repeat, every
+import schedule
 
 last_post_id = 0
-app = Flask(__name__)
 
-test_val = 1
-@app.route("/")
-@repeat(every(5).minutes)
-def index():
-    get_post()
-    return ""
 def write_msg(user_id, message):
     try:
         x = int(user_id)
@@ -37,15 +28,12 @@ def write_msg(user_id, message):
         )
 
 
-
-
-
 token = "vk1.a.hORY0Zv8EEUislonOv9OilF0EGo80NXQWgxrmZJNBtyw-Ki9uajoig3bnKInXZ94S6eXuzTpkRcRv0Z4crbgQE9np7J7NOrbFEX6zCHeLA2eHz4dTRi0-CqUS4iuhtHLCV85OHAkdhYy9YXIKq4doGPt2t90Tk70phUkMLMxV5XUNea37cj__Ep4Ex2jJ-C3LE6hvliJ4YOPWX8uRuserQ"
 
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 
-
+@schedule.repeat(schedule.every(5).minutes)
 def get_post():
     global last_post_id
     request = requests.get("https://vk.com/raspisanie_urtisi")
@@ -58,7 +46,7 @@ def get_post():
     x = wall_items[1].find('div', class_="wi_info").find("a")
     post_link = "https://vk.com/raspisanie_urtisi?w=" + x.attrs["href"][1:]
 
-    messenge = wall_items[1].find("div", class_="pi_text")
+    messenge = wall_items[1].find("div", class_="pi_text").lower()
 
     members = vk.groups.getMembers(group_id=217049074)['items']
     # print(vk.wall.get(group_id="raspisanie_urtisi"))
@@ -67,24 +55,34 @@ def get_post():
     # chat_info = vk.messages.getChat(chat_id=index)
     # print(chat_info)
 
-    if post_id != last_post_id and "284" in messenge:
+    if post_id != last_post_id:
         last_post_id = post_id
 
-        for i in members:
-            try:
-                write_msg(i, "хэй-хэй! учебный отдел опять что-то высрал! думаю, стоит чекнуть! " + post_link)
-            except Exception as e:
-                pass
-    # write_msg("shinomasson", "хэй-хэй! учебный отдел опять что-то высрал! думаю, стоит чекнуть! " + post_link)
-
-def main():
-    # schedule.run_pending()
-    # schedule.every(1).seconds.do(get_post)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-    # schedule.every(100).seconds.do(get_post)
-
+        new_messenge=""
+        if "284" in messenge:
+            new_messenge= "хэй-хэй! учебный отдел опять что-то высрал! думаю, стоит чекнуть! "
+        if "бурумбаев" in messenge and "адил" in messenge:
+            new_messenge = "учебный отдел написал что-от об Адиле(математика\физика) "
+        elif "чуркин" in messenge:
+            new_messenge= "учебный отдел написал что-то о Чуркине(русский) "
+        elif "белобородов" in messenge:
+            new_messenge= "учебный отдел написал что-то о Белобородовой(инфа) "
+        elif "савин" in messenge:
+            new_messenge= "учебный отдел написал что-то о Савиной(общество) "
+        elif "лаврентьев" in messenge:
+            new_messenge= "учебный отдел написал что-то о Лаврентьевой(англ 2 группа) "
+        elif "шабуров" in messenge:
+            new_messenge= "учебный отдел написал что-то о Шабуровой(история) "
+        
+        if new_messenge:
+            new_messenge += post_link
+            for i in members:
+                try:
+                    write_msg(i, new_messenge)
+                except Exception as e:
+                    pass
 
 if __name__ == '__main__':
-    main()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
